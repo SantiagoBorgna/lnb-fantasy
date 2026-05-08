494b9ac7ea69743e9eccefb4c6ae22829331491e
SantiagoBorgna <santiborgna5@gmail.com>
2026-05-08 20:26:46 -0300

    Rebuild

--- src/main/java/com/fantasy/lnb/LnbApplication.java
+++ src/main/java/com/fantasy/lnb/LnbApplication.java
@@ -1,5 +1,5 @@
 package com.fantasy.lnb;
-
+//para que compile de vuelta render
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
