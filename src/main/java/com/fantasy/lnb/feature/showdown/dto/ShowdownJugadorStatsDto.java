758fdf41b918a09dbcae341a20dba60890a87cd3
SantiagoBorgna <santiborgna5@gmail.com>
2026-08-10 19:15:50 -0300

    feat(showdown): include jersey data in ShowdownJugadorStatsDto

--- src/main/java/com/fantasy/lnb/feature/showdown/dto/ShowdownJugadorStatsDto.java
+++ src/main/java/com/fantasy/lnb/feature/showdown/dto/ShowdownJugadorStatsDto.java
@@ -1,1 +1,29 @@
-package com.fantasy.lnb.feature.showdown.dto; import lombok.Builder; import lombok.Data; @Data @Builder public class ShowdownJugadorStatsDto { private Long id; private String nombre; private String apellido; private String equipoSigla; private String posicion; private Boolean esCapitan; private Integer pts; private Integer reb; private Integer ast; private Integer stl; private Integer blk; private Integer tov; private Double valFantasy; private Double puntosAportados; }
+package com.fantasy.lnb.feature.showdown.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ShowdownJugadorStatsDto {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String equipoSigla;
+    private String equipoColorPrincipal;
+    private String equipoColorSecundario;
+    private String equipoModeloCamiseta;
+    private String numeroCamiseta;
+    private String posicion;
+    private Boolean esCapitan;
+    
+    private Integer pts;
+    private Integer reb;
+    private Integer ast;
+    private Integer stl;
+    private Integer blk;
+    private Integer tov;
+    
+    private Double valFantasy;
+    private Double puntosAportados;
+}
